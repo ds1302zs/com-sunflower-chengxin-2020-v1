b0325e638a9c5570ab624c4656f71b02fd3c6a62
ds1302zs <49970639+ds1302zs@users.noreply.github.com>
2020-01-01 16:05:33 +0800

    Add files via upload

---
+++ diegst.py
@@ -0,0 +1,29 @@
+import hashlib
+import math
+
+def exp_mode(base, exponent, n):
+    bin_array = bin(exponent)[2:][::-1]
+    r = len(bin_array)
+    base_array = []
+    pre_base = base
+    base_array.append(pre_base)
+    for _ in range(r - 1):
+        next_base = (pre_base * pre_base) % n
+        base_array.append(next_base)
+        pre_base = next_base
+    a_w_b = __multi(base_array, bin_array)
+    return a_w_b % n
+def __multi(array, bin_array):
+    result = 1
+    for index in range(len(array)):
+        a = array[index]
+        if not int(bin_array[index]):
+            continue
+        result *= a
+    return result
+print('请输入公钥及签名（合同请去除签名部分）')
+keyn=input()
+m=input()
+diegst=hashlib.sha256(open('hetong.txt','rb').read()).hexdigest()
+diegst1=exp_mode(m,65537,keyn)
+print(digest==digest1)
